4c55632918621a74a6688b9599efbbb64e2d89d5
mkit2009 <matejkotrba.skola@gmail.com>
2022-10-02 20:42:01 +0200

    Added my portfolio link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,36 @@ import styled from "styled-components";
 const StyledDiv = styled("div")`
   background-color: #0000008d;
   padding: 25px;
+
+  & > a {
+    position: relative;
+    text-decoration: none;
+    color: royalblue;
+
+    &::before {
+      content: "";
+      position: absolute;
+      bottom: -2px;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background-color: royalblue;
+      transform: scaleX(0);
+      transition: transform 0.4s ease;
+    }
+
+    &:hover::before {
+      transform: scaleX(1);
+    }
+  }
 `;
 
 function Footer() {
-  return <StyledDiv>Author: Matěj Kotrba</StyledDiv>;
+  return (
+    <StyledDiv>
+      Author: <a href="https://matejkotrba.vercel.app">Matěj Kotrba</a>
+    </StyledDiv>
+  );
 }
 
 export default Footer;
